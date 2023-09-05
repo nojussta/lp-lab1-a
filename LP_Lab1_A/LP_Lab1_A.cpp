a53a322e3620976b5efb506fd60722fec9f0ac97
Nojus Stasiunas <84374792+nojussta@users.noreply.github.com>
2023-09-05 19:40:27 +0300

    Add: Student struct, json parse data, output parsed data

--- LP_Lab1_A/LP_Lab1_A.cpp
+++ LP_Lab1_A/LP_Lab1_A.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 #include <iomanip>
 #include "json.hpp"
+#include <fstream>
+
+using namespace std;
+using json = nlohmann::json;
+
+// Define the Student struct
+struct Student {
+	string name;
+	int year;
+	double grade;
+	string hashed;
+};
 
 int main() {
+	ifstream inputFile("duomenys.json");
+	string jsonData((istreambuf_iterator<char>(inputFile)), istreambuf_iterator<char>());
+	json jsonStudents = json::parse(jsonData);
+	vector<Student> mainStudents;
+
+	// Parse student data from JSON
+	for (const auto& studentData : jsonStudents["students"]) {
+		Student student;
+		student.name = studentData["name"];
+		student.year = studentData["year"];
+		student.grade = studentData["grade"];
+		mainStudents.push_back(student);
+	}
+
+	for (const auto& student : mainStudents) {
+		cout << "Name: " << student.name << " Year: " << student.year << " Grade: " << student.grade << '\n';
+	}
 
 	return 0;
 }
